e5fe11ef06b8b65ed6f3d231cfff3c3cd0a620e1
mono <monotom@gmail.com>
2018-03-28 13:59:44 +0200

    dist v1.0.3

--- dist/jquery.animate.css.js
+++ dist/jquery.animate.css.js
@@ -2,10 +2,11 @@
   jQuery.fn.extend({
     animateCss: function(animationCssClass, duration, complete) {
       return this.each(function() {
-        var $this, durationSeconds;
+        var $this, durationCss, durationSeconds;
         durationSeconds = duration / 1000;
+        durationCss = (durationSeconds + "s").replace(',', '.');
         $this = $(this);
-        $this.css("animation-duration", durationSeconds + 's').addClass("animate " + animationCssClass);
+        $this.css("animation-duration", durationCss).addClass("animate " + animationCssClass);
         return setTimeout(function() {
           $this.removeClass("animate " + animationCssClass);
           if (complete) {
